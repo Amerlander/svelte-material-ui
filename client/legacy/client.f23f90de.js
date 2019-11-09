0a17930de6951ad3950d29e3073b7edbf00a94c5
Hunter Perrin <hperrin@gmail.com>
2019-11-08 16:00:52 -0800

    chore: update built files

---
+++ client/legacy/client.f23f90de.js
@@ -0,0 +1,34 @@
+import './index.7a4dc241.js';
+import { s as start } from './app.d6889210.js';
+import './style-inject.es.899c797a.js';
+import './index.ae461dd0.js';
+import '@babel/runtime/helpers/esm/classCallCheck';
+import '@babel/runtime/helpers/esm/createClass';
+import '@babel/runtime/helpers/esm/possibleConstructorReturn';
+import '@babel/runtime/helpers/esm/getPrototypeOf';
+import '@babel/runtime/helpers/esm/assertThisInitialized';
+import '@babel/runtime/helpers/esm/inherits';
+import '@babel/runtime/helpers/esm/defineProperty';
+import '@babel/runtime/helpers/esm/typeof';
+import '@babel/runtime/helpers/esm/toConsumableArray';
+import './useActions.c1147f63.js';
+import '@babel/runtime/helpers/esm/construct';
+import './Div.a61f6e67.js';
+import './Span.7dac0ff6.js';
+import './index.133000a7.js';
+import './H2.a0f7c78e.js';
+import './index.e5fed2b4.js';
+import '@babel/runtime/helpers/esm/slicedToArray';
+import './bare.0de77689.js';
+import './Button.5bef6ac5.js';
+import './Label.7dcfe483.js';
+import './Icon.cf661a2d.js';
+import './index.e0046081.js';
+import './index.35341f63.js';
+import '@babel/runtime/regenerator';
+import '@babel/runtime/helpers/esm/asyncToGenerator';
+import './Item.759220d0.js';
+
+start({
+  target: document.getElementsByTagName('app')[0]
+});
